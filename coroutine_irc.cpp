e4b8319b243a98b0907639803b474d021b7d158b
Vinzenz Feenstra <evilissimo@gmail.com>
2012-08-12 21:49:00 +0200

    Little rearrangement - async resolve + async connect within the class

--- coroutine_irc.cpp
+++ coroutine_irc.cpp
@@ -61,27 +61,46 @@ struct irc : coroutine
         {
         }
 
+        void operator()( error_code const & ec, tcp::resolver::iterator iter ) 
+        {
+            (*instance_)(ec, iter);
+        }
         void operator()( error_code const & ec = error_code(), size_t count = 0 ) 
         {
-                (*instance_)(ec, count);
+            (*instance_)(ec, count);
         }
     private:
         irc * instance_;
     };
 
-    irc(io_service & service, tcp::endpoint ep)
+    irc(io_service & service, std::string const & server, std::string const & port )
     : coroutine()
     , io_( service )
     , socket_( io_ )
-    , endpoint_( ep )
+    , resolver_( io_ )
+    , endpoint_()
     , readbuffer_()
     , send_buffer_()
     , wrapped_()
+    , server_(server)
+    , port_(port)
+    , endpoint_iterator_()
     {
         wrapped_ = wrapped_impl( this );
-        socket_.async_connect( ep, wrapped_ );
+
+        tcp::resolver::query query( server, port );
+        resolver_.async_resolve( query, wrapped_ );
     }
 
+
+    void operator()( 
+        error_code const & ec, 
+        tcp::resolver::iterator iter ) 
+    {
+        std::cout << "Host resolved" << std::endl;
+        endpoint_iterator_ = iter;
+        (*this)( ec );
+    }
 
     void operator()( 
         error_code const & ec = error_code(),
@@ -89,9 +108,29 @@ struct irc : coroutine
     {
         reenter (this)
         {
-        entry:            
-            std::cout << "Entering coroutine" << std::endl;
+        entry:
+            if( !ec ) 
+            {
+                if( endpoint_iterator_ != tcp::resolver::iterator() )
+                {
+                    endpoint_ = *endpoint_iterator_;
+                    std::cout << "Trying to connect to: " << endpoint_ << std::endl;
+                    yield socket_.async_connect( endpoint_, wrapped_ );
+                }
+                else
+                {
+                    std::cout << "Failed to connect to host: " << server_ << ":" << port_ << ". Message: " << ec.message() << std::endl;
+                    break;
+                }
+            }
+            else
+            {
+                std::cout << "Failed to resolve host: " << server_ << ":" << port_ << ". Message: " << ec.message() << std::endl;
+                break;
+            }
+
             send_buffer_ = "NICK evilbitch\r\nUSER evilbitch 0 * :evilbitch\r\n";
+            std::cout << "Sending: " << send_buffer_ << std::flush;
             yield boost::asio::async_write( socket_, buffer(send_buffer_), wrapped_ );
 
             while( !ec )
@@ -121,19 +160,20 @@ struct irc : coroutine
 private:
     io_service & io_;
     tcp::socket socket_;
+    tcp::resolver resolver_;
     tcp::endpoint endpoint_;
     boost::asio::streambuf readbuffer_;
     std::string send_buffer_;
     wrapped_impl wrapped_;
+    std::string server_;
+    std::string port_;
+    tcp::resolver::iterator endpoint_iterator_;
 };
 
 int main()
 {
     io_service service;
-    tcp::resolver resolver( service );
-    tcp::resolver::query query( "irc.euirc.net", "6667" );
-    tcp::resolver::iterator it = resolver.resolve( query );
-    irc instance( service, *it ); 
+    irc instance( service, "irc.euirc.net", "6667"  ); 
     service.run();
 }
 
